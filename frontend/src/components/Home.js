b42482a5f50cc03cdc2dc44cdd5bb42dea500258
GGryg <shiryon@wp.pl>
2023-01-15 19:50:05 +0100

    frontend - added main page

---
+++ frontend/src/components/Home.js
@@ -0,0 +1,16 @@
+import PostsList from "./PostsList";
+
+const Home = () => {
+    return (
+        <div>
+            <div className='bg-dark-lighter w-full h-10'></div>
+            <div className='bg-dark w-full px-40 p-2'>
+                <h1 className='text-gray-200 text-5xl'>Home</h1>
+            </div>
+            <div className='bg-dark px-40 py-3 h-screen'>
+                <PostsList />
+            </div>
+        </div>)
+};
+
+export default Home;
