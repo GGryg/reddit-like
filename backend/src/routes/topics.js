3465224ee33ad6cf2341debe17842a3952bb307b
GGryg <shiryon@wp.pl>
2023-01-15 20:02:53 +0100

    backend - added route for topics

--- backend/src/routes/topics.js
+++ backend/src/routes/topics.js
@@ -3,15 +3,15 @@ const router = express.Router();
 
 const dbo = require('./../db/conn');
 
-router.route('/posts').get((req, res) => {
+router.route('/topics').get((req, res) => {
     const dbConnect = dbo.getDb();
 
     dbConnect
-        .collection("posts")
+        .collection("topics")
         .find({})
         .toArray((err, result) => {
             if (err){
-                res.status(400).send("Error fetching posts");
+                res.status(400).send("Error fetching topics");
             }
             else{
                 res.json(result);
