05eeb64d7f88a566e881a129164baa05cffb7a7e
GGryg <shiryon@wp.pl>
2023-01-15 12:22:13 +0100

    backend - fix

--- backend/index.js
+++ backend/index.js
@@ -21,7 +21,7 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(cookieParser());
 
 app.use(require('./src/routes/users'));
-app,use(require('./src/routes/posts'));
+app.use(require('./src/routes/posts'));
 
 app.listen(port, () => {
     dbo.connectToServer((err) => {
