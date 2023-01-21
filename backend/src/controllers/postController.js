2972ebdc953651f292d187998cc1f70e5b3237d3
GGryg <shiryon@wp.pl>
2023-01-21 21:29:42 +0100

    backend - rewritten posts to mongoose

---
+++ backend/src/controllers/postController.js
@@ -0,0 +1,142 @@
+const Post = require('../models/PostSchema');
+
+const getPosts = async (req, res) => {
+    const { sort, filter } = req.query;
+    const sortQuery = sort || {};
+    const filterQuery = filter || {};
+
+    try{
+        const posts = await Post.find(filterQuery).sort(sortQuery);
+
+        return res.status(200).json(posts);
+    }
+    catch (err) {
+        console.error(err);
+        return res.status(500).json('error');
+    }
+};
+
+const getPost = async (req, res) => {
+    const { id } = req.params;
+
+    try{
+        const post = await Post.findById(id);
+        if(!post)
+            return res.status(404).json('Post not found');
+
+        return res.status(200).json(post);
+    }
+    catch (err) {
+        console.error(err);
+        return res.status(500).json('error');
+    }
+};
+
+const getCurrentPosts = async (req, res) => {
+    const { id } = req.user;
+
+    try{
+        const post = await Post.find({user: id});
+        if(!post)
+            return res.status(404).json('Post not found');
+
+        return res.status(200).json(post);
+    }
+    catch (err) {
+        console.error(err);
+        return res.status(500).json('error');
+    }
+};
+
+const createPost = async (req, res) => {
+    const { id } = req.user;
+    const { title, content, links, picture, topic } = req.body;
+
+    if(!title)
+        return res.status(400).json('There is missing field');
+
+    const post = {title: title, user: id, topic: topic};
+    if(content)
+        post.content = content;
+    if(links)
+        post.links = links;
+    if(picture)
+        post.picture = picture;
+    // fix picture later
+
+    try{
+        const newPost = new Post(post);
+        await newPost.save();
+
+        return res.status(201).json('Added a new post');
+    }
+    catch (err) {
+        console.error(err);
+        return res.status(500).json('error');
+    }
+};
+
+const updatePost = async (req, res) => {
+    const { id } = req.params;
+    try{
+        const post = await Post.findById(id);
+        if(!post)
+            return res.status(404).json('Post not found');
+        if(post.user.toString() !== req.user.id || req.user.role !== 'admin' || req.user.role !== 'moderator')
+            return res.status(403).json('Not authorized')
+        
+        const { title, content, links, picture } = req.body;
+
+        if(!title && !content && !links && !picture)
+            return res.status(400).json('There are missing fields');
+        
+        const updatedValues = {};
+        if(title)
+            updatedValues.title = title;
+        if(content)
+            updatedValues.content = content;
+        if(links)
+            updatedValues.links = links;
+        if(picture)
+            updatedValues.picture = picture;
+        // fix picture later
+
+        const updatedPost = await Post.findByIdAndUpdate(id, updatedValues, {new: true});
+        if(!updatedPost)
+            return res.status(404).json('Post not found');
+        
+        return res.status(200).json(updatedPost);
+    }
+    catch (err) {
+        console.error(err);
+        return res.status(500).json('error');
+    }
+};
+
+const deletePost = async (req, res) => {
+    const { id } = req.params;
+
+    try{
+        const post = await Post.findById(id);
+        if(!post)
+            return res.status(404).json('Post not found');
+        if(post.user.toString() !== req.user.id || req.user.role !== 'admin' || req.user.role !== 'moderator')
+            return res.status(403).json('Not authorized')
+
+        await Post.findByIdAndDelete(id);
+        return res.status(200).json('Post has been deleted');
+    }
+    catch (err) {
+        console.error(err);
+        return res.status(500).json('error');
+    }
+};
+
+module.exports = {
+    getPosts,
+    getPost,
+    getCurrentPosts,
+    createPost,
+    updatePost,
+    deletePost,
+};
